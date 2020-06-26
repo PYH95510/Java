abc3fbf41ac7759fba39fcd2c651ef5527ad4e11
PYH95510 <44890698+PYH95510@users.noreply.github.com>
2020-06-26 16:16:50 -0400

    Add files via upload

---
+++ ObjectOrientedDesign/IntMod.java
@@ -0,0 +1,7 @@
+package hw214_2;
+
+//Integer modulo m (m is a prime number)
+public class IntMod implements Field, Ordered {
+    public IntMod(int n, int m) {
+    }
+}
---
+++ ObjectOrientedDesign/PolyDbl.java
@@ -0,0 +1,10 @@
+package hw214_2;
+
+public class PolyDbl implements Ring, Modulo, Ordered {
+    private double[] coef;
+    public PolyDbl(double[] coef) {
+        //TODO: implement this constructor
+        //      leading zeros should be trimmed off
+    }
+    //TODO: implement the rest
+}
